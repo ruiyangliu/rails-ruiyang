e2354a3edfb80d438944663bd94c07d9bfc4d4ab
Ruiyang Liu <contact@ruiyang.ca>
2013-05-16 15:01:08 -0400

    Ran rails new command. Hello World.

---
+++ ruiyang_site/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RuiyangSite::Application.config.secret_token = '98ae0bdef27e03a86726bda96ddcc21654bd817e1f176d5a6a1b023ca98051d77f96792825f69a95f612541a99a6a5e2956ec3c7b7cccfdab6df76aaa32436d8'
